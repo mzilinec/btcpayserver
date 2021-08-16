34af759e367279680115a81bc2ed5488dbb9918b
Kukks <evilkukka@gmail.com>
2021-08-16 10:07:49 +0200

    wip

---
+++ BTCPayServer/LNURL/LNURLController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using BTCPayServer.Services.Invoices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTCPayServer
+{
+    [Route("~/stores/{storeId}/[controller]")]
+    public class LNURLController : Controller
+    {
+        private readonly InvoiceRepository _invoiceRepository;
+
+        public LNURLController(InvoiceRepository invoiceRepository)
+        {
+            _invoiceRepository = invoiceRepository;
+        }
+        
+        
+        [HttpGet("pay/{invoiceId}")]
+        public async Task<IActionResult> GetLNURLForInvoice(string invoiceId)
+        {
+            var i = await _invoiceRepository.GetInvoice(invoiceId, true);
+            LNURL.LNURLPayRequest
+        } 
+        
+    }
+}
